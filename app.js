227cfc85ef03f6dcd6a0ba0d915931f2d332e29e
xan <nukeninuchiha@yandex.ru>
2023-03-05 14:49:07 +0300

    init

---
+++ app.js
@@ -0,0 +1,75 @@
+let edits = document.querySelectorAll('.edit img')
+
+edits.forEach(edit => {
+    let newEdit = edit.cloneNode(true)
+    let newEdit2 = edit.cloneNode(true)
+    let newEdit3 = edit.cloneNode(true)
+    let newEdit4 = edit.cloneNode(true)
+    let newEdit5 = edit.cloneNode(true)
+    let newEdit6 = edit.cloneNode(true)
+    let newEdit7 = edit.cloneNode(true)
+    let newEdit8 = edit.cloneNode(true)
+    let newEdit9 = edit.cloneNode(true)
+    let newEdit10 = edit.cloneNode(true)
+    let newEdit11 = edit.cloneNode(true)
+    let newEdit12 = edit.cloneNode(true)
+    let newEdit13 = edit.cloneNode(true)
+    let newEdit14 = edit.cloneNode(true)
+    let newEdit15 = edit.cloneNode(true)
+    let newEdit16 = edit.cloneNode(true)
+    let newEdit17 = edit.cloneNode(true)
+    let newEdit18 = edit.cloneNode(true)
+    let newEdit19 = edit.cloneNode(true)
+    let newEdit20 = edit.cloneNode(true)
+
+    console.log(newEdit);
+
+    // newEdit.style.top = '-720px'
+    // newEdit.style.transform = 'scale(0.5, -0.5)'
+
+    // newEdit2.style.top = '-720px'
+    // newEdit2.style.right = '-720px'
+    // newEdit2.style.transform = 'scale(-0.5, -0.5)'
+
+    // newEdit3.style.right = '-720px'
+    // newEdit3.style.transform = 'scale(-0.5, 0.5)'
+
+    // newEdit4.style.top = '720px'
+    // newEdit4.style.right = '-720px'
+    // newEdit4.style.transform = 'scale(-0.5, -0.5)'
+
+    // newEdit5.style.top = '720px'
+    // newEdit5.style.transform = 'scale(0.5, -0.5)'
+
+    // newEdit6.style.top = '720px'
+    // newEdit6.style.left = '-720px'
+    // newEdit6.style.transform = 'scale(-0.5, -0.5)'
+
+    // newEdit7.style.left = '-720px'
+    // newEdit7.style.transform = 'scale(-0.5, 0.5)'
+
+    // newEdit8.style.top = '-720px'
+    // newEdit8.style.left = '-720px'
+    // newEdit8.style.transform = 'scale(-0.5, -0.5)'
+
+    edit.after(newEdit)
+    edit.after(newEdit2)
+    edit.after(newEdit3)
+    edit.after(newEdit4)
+    edit.after(newEdit5)
+    edit.after(newEdit6)
+    edit.after(newEdit7)
+    edit.after(newEdit8)
+    edit.after(newEdit9)
+    edit.after(newEdit10)
+    edit.after(newEdit11)
+    edit.after(newEdit12)
+    edit.after(newEdit13)
+    edit.after(newEdit14)
+    edit.after(newEdit15)
+    edit.after(newEdit16)
+    edit.after(newEdit17)
+    edit.after(newEdit18)
+    edit.after(newEdit19)
+    edit.after(newEdit20)
+})
